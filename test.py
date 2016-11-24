1e26dfbd187db7fe7e568ea3175e4a5b33489ed2
OromP <nguyenvinhyphat@yahoo.com.vn>
2016-11-24 18:03:11 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ def welcome():
 			print "ID = %d"% (p1.id)
 			print "Name = %s"% (p1.name)
 			print "Type = %s"% (p1.type)
+			print "Speed = %d"% (p1.speed)
 			print "Base Hp = %d"%(p1.base_hp)
 			print "Base Attack = %d"% (p1.base_atk)
 			print "Base Defense = %d"% (p1.base_def)
@@ -21,6 +22,7 @@ def welcome():
 			print "ID = %d"% (p2.id)
 			print "Name = %s"% (p2.name)
 			print "Type = %s"% (p2.type)
+			print "Speed = %d"% (p2.speed)
 			print "Base Hp = %d"%(p2.base_hp)
 			print "Base Attack = %d"% (p2.base_atk)
 			print "Base Defense = %d"% (p2.base_def)
@@ -29,56 +31,75 @@ def welcome():
 		else:
 				print "Invalid input.\n" 	
 class Pokemon(object):
-	def __init__ (self,id,name,type,base_hp,base_atk,base_def ):
+	def __init__ (self,id,name,type,speed,base_hp,base_atk,base_def ):
   		self.id=id
 		self.name=name
 		self.type=type
+		self.speed = speed
 		self.base_hp = base_hp
 		self.base_atk=base_atk
 		self.base_def=base_def
 def text():
-	print "You chose your first pokemon.\n"
+	print "\nYou chose your first pokemon."
 	print "The other player will play the remaining pokemon.\n"
-	print "Player 1 will attack first\n."
-
+ 
 def text1():
 	ask = raw_input("Do you want to attack?(Y/N)\n")
 	if ask == "Y" :
 		p2.base_hp = p2.base_hp - p1.base_atk
+		print "You attacked Player 2!"
 	elif ask == "N" :
 	 	print "You didnt attack"
-			
+	print "Player 1 HP: %s " %(p1.base_hp)
+	print "Player 2 HP: %s\n "% (p2.base_hp) 	
+def iniAttack():
+	if p1.speed > p2.speed:
+		print "Player 1 will attack first."
+		return   1 	
+	elif p1.speed < p2.speed:		
+		print "Player 2 will attack first."
+		return   2	
+		 
+	
 def text2():
 	ask = raw_input("Do you want to attack?(Y/N)\n")
 	if ask == "Y" :
 		p1.base_hp = p1.base_hp - p2.base_atk	
+		print "You attacked Player 1!"
 	elif ask == "N" :
 		print "You didnt attack\n"
+	print "Player 1 HP: %s "% (p1.base_hp)
+	print "Player 2 HP: %s\n "% (p2.base_hp) 		
 	 	 	
 def endgame():
 	if p1.base_hp == 0 | p1.base_hp <0:
 		print "Game Over, Player 2 wins.\n"
 	elif p2.base_hp == 0 | p2.base_hp <0:
 		print "Game Over, Player 1 wins.\n"	
-	print "Player 1 hp: %d\n" %(p1.base_hp)
-	print "Player 2 hp: %d\n"%(p2.base_hp)	
+	print "Final Player 1 hp: %d\n" %(p1.base_hp)
+	print "Final Player 2 hp: %d\n"%(p2.base_hp)	
 
 
-p1 = Pokemon(1,"Ivysaur","grass",250,60,50 )
-p2 = Pokemon(2,"Chameleon","fire",200,60,45 )	 
-
-welcome()
+p1 = Pokemon(1,"Ivysaur","grass",50,250,60,50 )
+p2 = Pokemon(2,"Chameleon","fire",60,200,60,45 )	 
+eV = 0.5 
  
-turn = 1
-while p1.base_hp > 0 | p2.base_hp >0:
-
+welcome()
+turn = iniAttack()
+while p1.base_hp > 0 | p2.base_hp > 0:	
+	
 	if turn == 1:
 		print "Its player 1's turn.\n"
 		text1()
-		turn = 2
-
-	if turn == 2:
+		if p2.base_hp <= 0:
+			break
+		else:		
+			turn = 2
+	elif turn == 2:
 		print "Its player 2's turn.\n"
 		text2()
-		turn = 1	
+		if p1.base_hp <= 0:
+			break
+		else:	
+			turn = 1	
 endgame()		
